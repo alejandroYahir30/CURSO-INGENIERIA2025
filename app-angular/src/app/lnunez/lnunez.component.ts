323b75d7fd10737dda97e7b44037adb8cf0f93b7
LeosterCode <164199518+LeosterCode@users.noreply.github.com>
2025-10-16 18:26:16 -0600

    Merge del componente lnunez

---
+++ app-angular/src/app/lnunez/lnunez.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lnunez',
+  imports: [],
+  templateUrl: './lnunez.component.html',
+  styleUrl: './lnunez.component.css',
+})
+export class LnunezComponent { }
